c312446569f7b6d4f71659db706f6474a8d24863
visstt <egorskomorohov020606@gmail.com>
2024-11-29 23:37:22 +0500

    create header

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './Pages/Home/HomePage';
+function App() {
+  return (
+    <>
+<Router>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+      </Routes>
+    </Router>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './Header.module.css'
+import Logo from './Logo.png'
+
+export default function Header() {
+  return (
+    <div className={styles.header}>
+        <div className={styles.logoSection}>
+            <img src={Logo} alt=""  className={styles.logo}/>
+            <h1>SIMPLE MONEY</h1>
+        </div>
+        <div className={styles.loginSection}>
+                <a href='#'><h1>Вход</h1></a>
+                <a href='#'><h1 className={styles.reg}>Регистрация</h1></a>
+        </div>
+    </div>
+  )
+}
